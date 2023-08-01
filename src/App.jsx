fcfc4c68814aca3458cde86435ddb8c18f47a517
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-08-01 13:44:35 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <h1>Welcome to BMI Calculator</h1>
+      </div>
+      <div className="">
+        {/* user input container here */}
+        <div className=""></div>
+        {/* bmi number and circle progress bar here */}
+        <div className=""></div>
+        {/* explanation container here */}
+        <div className=""></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
